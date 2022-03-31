6b4abe3f0a42e24df405bfc4150cc7c6c5f8de45
whitebeach1993 <ryosukeshirahama0209@gmail.com>
2022-03-31 22:49:17 +0900

    refer to transaction data

--- index.py
+++ index.py
@@ -9,24 +9,41 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_csv("data/transaction.csv", encoding="cp932")
+df["日付"] = pd.to_datetime(df['日付'], format='%Y-%m-%d')
+df["年"] = df['日付'].dt.year
+df["月"] = df['日付'].dt.month
+df["金額"] = df['金額'].astype(int)
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+df_sum = df.groupby(["顧客名", "商品名"]).sum().sort_values(
+    ["顧客名", "金額"], ascending=False).reset_index()
+
+
+transction_scatter = px.scatter(df_sum, x="数量", y="単価", color="顧客名",
+                                size='金額', hover_data=['商品名'])
+
+df_time = df.groupby(["顧客名", "年", "月"]).sum().sort_values(
+    ["顧客名", "金額"], ascending=False).reset_index()
+df_time["日付"] = pd.to_datetime(df_time['年'].astype(
+    str)+df_time['月'].astype(str), format='%Y%m')
+df_time = df_time.sort_values(["顧客名", "日付"])
+transction_line = px.line(df_time, x="日付", y="金額", color="顧客名")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='仕分けの可視化'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        仕分けの情報を可視化することで不正な取引のあたりをつける
     '''),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='transction_scatter',
+        figure=transction_scatter
+    ),
+
+    dcc.Graph(
+        id='transction_line',
+        figure=transction_line
     )
 ])
 

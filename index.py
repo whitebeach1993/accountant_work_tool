57b1be9abc50b24abf618b8508ff653e924a3e29
whitebeach1993 <ryosukeshirahama0209@gmail.com>
2022-04-02 19:20:49 +0900

    add bootstrap

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
+from pydoc import pager
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
+from dash import dash_table
 import plotly.express as px
 import pandas as pd
+import dash_bootstrap_components as dbc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(external_stylesheets=[dbc.themes.SIMPLEX])
 server = app.server
 
 df = pd.read_csv("data/transaction.csv", encoding="cp932")
@@ -29,23 +32,75 @@ df_time["日付"] = pd.to_datetime(df_time['年'].astype(
 df_time = df_time.sort_values(["顧客名", "日付"])
 transction_line = px.line(df_time, x="日付", y="金額", color="顧客名")
 
-app.layout = html.Div(children=[
-    html.H1(children='仕分けの可視化'),
+view_columns = ['日付', '顧客名', '商品名', '分類', '単価', '数量', '金額']
 
-    html.Div(children='''
-        仕分けの情報を可視化することで不正な取引のあたりをつける
-    '''),
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H1("仕分けの可視化"),
+                )
+            ],
+            className="h-30"
+        ),
+        dbc.Row(
+            [
+                # dbc.Col(
+                #     html.H4("サンプルデータ"),
+                #     dash_table.DataTable(
+                #         id='table',
+                #         columns=[{"name": i, "id": j}
+                #                  for i, j in zip(df[view_columns], view_columns)],
+                #         data=df.to_dict('records'),
+                #         page_size=5,
+                #         sort_action='native',
+                #     ),
+                #     width=6,
+                #     style={"height": "100%", "size": "10px"},
+                # ),
+                dbc.Col(
+                    html.H4("サンプルデータの顧客ごとの集計値"),
+                    width=6,
+                    style={"height": "100%", },
+                ),
+            ],
+            className="h-50"
+        ),
 
-    dcc.Graph(
-        id='transction_scatter',
-        figure=transction_scatter
-    ),
+        dbc.Row(
+            [
+                # dbc.Col(
+                #     html.H4("取引先ごとの数量と単価(大きさは金額)"),
+                #     dcc.Graph(
+                #         # id='transction_scatter',
+                #         config={
+                #             'displayModeBar': False,
+                #         },
+                #         figure=transction_scatter
+                #     ),
 
-    dcc.Graph(
-        id='transction_line',
-        figure=transction_line
-    )
-])
+                #     width=6,
+                #     style={"height": "100%", },
+                # ),
+                dbc.Col(
+                    dcc.Graph(
+                        # id='transction_line',
+                        config={
+                            'displayModeBar': False,
+                        },
+                        figure=transction_line
+                    ),
+                    width=6,
+                    style={"height": "100%", },
+                ),
+            ],
+            className="h-50",
+        ),
+    ],
+    style={"height": "90vh"},
+)
+
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True, use_reloader=True)

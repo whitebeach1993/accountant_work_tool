0b6ae2a2294d20285641028d4c8f036addb038b8
whitebeach1993 <ryosukeshirahama0209@gmail.com>
2022-04-03 11:23:00 +0900

    insert graph and first output

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
-from pydoc import pager
 import dash
 from dash import dcc
-from dash import html
+import dash_html_components as html
 from dash import dash_table
 import plotly.express as px
 import pandas as pd
@@ -9,18 +8,27 @@ import dash_bootstrap_components as dbc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(external_stylesheets=[dbc.themes.SIMPLEX])
-server = app.server
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.SIMPLEX],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ],
+)
+# server = app.server
 
 df = pd.read_csv("data/transaction.csv", encoding="cp932")
+view_columns = ['日付', '顧客名', '商品名', '分類', '単価', '数量', '金額']
+df = df[view_columns]
+# df = df.head()
 df["日付"] = pd.to_datetime(df['日付'], format='%Y-%m-%d')
 df["年"] = df['日付'].dt.year
 df["月"] = df['日付'].dt.month
 df["金額"] = df['金額'].astype(int)
 
-df_sum = df.groupby(["顧客名", "商品名"]).sum().sort_values(
-    ["顧客名", "金額"], ascending=False).reset_index()
-
+df_sum = df.groupby(["顧客名", "商品名", "単価"]).sum().sort_values(
+    ["金額"], ascending=False).reset_index()
+view_columns = ['顧客名', '商品名', '単価', '数量', '金額']
+df_sum = df_sum[view_columns]
 
 transction_scatter = px.scatter(df_sum, x="数量", y="単価", color="顧客名",
                                 size='金額', hover_data=['商品名'])
@@ -32,75 +40,82 @@ df_time["日付"] = pd.to_datetime(df_time['年'].astype(
 df_time = df_time.sort_values(["顧客名", "日付"])
 transction_line = px.line(df_time, x="日付", y="金額", color="顧客名")
 
-view_columns = ['日付', '顧客名', '商品名', '分類', '単価', '数量', '金額']
-
+print("--------------------------------------")
 app.layout = dbc.Container(
-    [
+    [dbc.Row(
+        [
+            dbc.Col(
+                html.H1("仕分けの全体像の可視化", className='text-center text-primary mb-4'),
+                width=12
+            )
+        ],
+        className="h-30",
+    ),
         dbc.Row(
             [
-                dbc.Col(
-                    html.H1("仕分けの可視化"),
-                )
-            ],
-            className="h-30"
-        ),
-        dbc.Row(
-            [
-                # dbc.Col(
-                #     html.H4("サンプルデータ"),
-                #     dash_table.DataTable(
-                #         id='table',
-                #         columns=[{"name": i, "id": j}
-                #                  for i, j in zip(df[view_columns], view_columns)],
-                #         data=df.to_dict('records'),
-                #         page_size=5,
-                #         sort_action='native',
-                #     ),
-                #     width=6,
-                #     style={"height": "100%", "size": "10px"},
-                # ),
-                dbc.Col(
-                    html.H4("サンプルデータの顧客ごとの集計値"),
+                dbc.Col([
+                    html.H4("サンプルデータ"),
+                    dash_table.DataTable(df.to_dict('records'),
+                                         [{"name": i, "id": i}
+                                             for i in df.columns],
+                                         page_size=5,
+                                         )
+
+                ],
                     width=6,
-                    style={"height": "100%", },
+                    style={"height": "100%"},
+
+                ),
+                dbc.Col([
+                    html.H4("サンプルデータの顧客名と商品名ごとの金額の合計値"),
+                    dash_table.DataTable(df_sum.to_dict('records'),
+                                         [{"name": i, "id": i}
+                                             for i in df_sum.columns],
+                                         page_size=5,
+                                         )
+
+                ],
+                    width=6,
+                    style={"height": "100%"},
+
                 ),
             ],
-            className="h-50"
-        ),
-
+            # className="h-8",
+    ),
         dbc.Row(
             [
-                # dbc.Col(
-                #     html.H4("取引先ごとの数量と単価(大きさは金額)"),
-                #     dcc.Graph(
-                #         # id='transction_scatter',
-                #         config={
-                #             'displayModeBar': False,
-                #         },
-                #         figure=transction_scatter
-                #     ),
-
-                #     width=6,
-                #     style={"height": "100%", },
-                # ),
-                dbc.Col(
+                dbc.Col([
+                    html.H4("取引（単価×数量)と取引先との関係"),
                     dcc.Graph(
-                        # id='transction_line',
+                        id='transction_scatter',
                         config={
                             'displayModeBar': False,
                         },
-                        figure=transction_line
-                    ),
+                        figure=transction_scatter,
+                    ), ],
                     width=6,
-                    style={"height": "100%", },
+                ),
+
+                dbc.Col([
+                    html.H4("時期別取引金額の推移"),
+                    dcc.Graph(
+                        id='transction_line',
+                        config={
+                            'displayModeBar': False,
+                        },
+                        figure=transction_line,
+                    ),
+                ],
+                    width=6,
+
                 ),
             ],
-            className="h-50",
-        ),
+            className="h-30"
+    ),
+
     ],
     style={"height": "90vh"},
 )
-
 
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=True)
